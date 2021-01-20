8b1fc8699e1af657f417afbfc2ec52736513b0ff
Matan Kadosh <kadosh10001@gmail.com>
2021-01-20 14:40:21 +0200

    Add gitignore Add further capabilities for build triggering Better error handling Update readme Bump version to 2.0.0 Update zip

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # kaholo-plugin-jenkins
 Jenkins plugin for Kaholo
+
+This plugin is a wrapper for Jenkins RestAPI. 
+The plugin is using user's token for authenticating its requests.
+
+Further information on user's token and authenticating requests can be found on [Jenkins documentation](https://www.jenkins.io/doc/book/system-administration/authenticating-scripted-clients/).
+
+## Settings ##
+
+* Jenkins Url - The url of your jenkins
+* Username - The user to trigger the builds
+* Token - The user token item in Kaholo's vault
+
+
+## Method: Build job
+
+**Description**
+
+Triggers a job build on Jenkins.
+
+**Parameters**
+
+* Job name - The name of the job to build.
+* Parameters (Object) - The build parameters. A key-value object refrenced from code layer. 
+* Wait for build end - Determine if Kaholo should wait for the build to finish or not. Default: false
+* Fail on build failure - Determines if Kaholo should fail the action in case the jenkins build fails. This works only if `Wait for build end` parameter is marked as `treu`. Default: false
--- src/app.js
+++ src/app.js
@@ -1,42 +1,89 @@
 var jenkins = require('jenkins'); 
 
-function _initJenkins(action){
-	return jenkins({ baseUrl: action.params.URL, crumbIssuer: true });
-}
-
-function _handleParams(PARAMETERS){
-	if (typeof PARAMETERS == 'string')
-		return JSON.parse(PARAMETERS);
-	else 
-		return PARAMETERS;
-}
-
-function buildJob(action){
-	let _jenkins = _initJenkins(action);
-	return new Promise((resolve, reject) => {
-		let buildOptions = { 
-			name: action.params.JOB,
-			token: action.params.TOKEN,
-		};
-
-		if(action.params.PARAMETERS){
-			try{
-				buildOptions.parameters = _handleParams(action.params.PARAMETERS);
-			} catch (err) {
-				return reject(new Error("Error parsing parmeters : " + err.message))
-			}
+function getJenkinsClient(settings){
+	return jenkins({ 
+		baseUrl: settings.URL,
+		headers: {
+			Authorization: `Basic ${Buffer.from(settings.user + ':' + settings.TOKEN).toString('base64')}`
 		}
+	});
+}
 
-		_jenkins.job.build(buildOptions, function(err, data) {
+async function waitOnQueue(jenkinsClient, buildQueueNumber) {
+	const buildNumber = await new Promise((resolve,reject)=>{
+		jenkinsClient.queue.item(buildQueueNumber, async function(err, item) {
+		  if (err) return reject(err);
+		  
+		  if (item.executable) {
+			return resolve(item.executable.number);
+		  }
+
+		  if (item.cancelled) {
+			return reject('cancelled');
+		  }
+		  resolve(null)
+		});
+	});
+
+	// If got buildNumber
+	if(buildNumber!==null){
+		return buildNumber;
+	}
+
+	// Wait 500ms and check again
+	await new Promise((timeoutResolve)=>setTimeout(timeoutResolve,500));
+	return waitOnQueue(jenkinsClient, buildQueueNumber);
+}
+
+async function waitUntilEnd(jenkinsClient, job, buildNumber) {
+	const build = await new Promise((resolve,reject)=>{
+		jenkinsClient.build.get(job, buildNumber, async function(err, item) {
+		  if (err) return reject(err);
+		  resolve(item);
+		});
+	});
+
+	if (build.result){
+		return build;
+	}
+	// Wait 500ms and check again
+	await new Promise((timeoutResolve)=>setTimeout(timeoutResolve,500));
+	return waitUntilEnd(jenkinsClient, job, buildNumber);
+}
+
+async function buildJob(action, settings){
+	let jenkinsClient = getJenkinsClient(settings);
+	let buildOptions = { 
+		name: action.params.JOB
+	};
+	
+	if(action.params.PARAMETERS){
+		buildOptions.parameters = action.params.PARAMETERS;
+	}
+
+	const buildQueueNumber = await new Promise((resolve, reject) => {
+		jenkinsClient.job.build(buildOptions, function(err, data) {
 			if (err) {
-				console.log('hey')
 				return reject(err)
 			};
 			return resolve(data);
 		});
 	})
-}
 
+	if (!action.params.waitForEnd || action.params.waitForEnd === "false"){
+		return {queueNumber: buildQueueNumber};
+	}
+
+	const buildNumber = await waitOnQueue(jenkinsClient,buildQueueNumber);
+
+	const build = await waitUntilEnd(jenkinsClient, buildOptions.name, buildNumber);
+	
+	if(build.result=='FAILURE' && (action.params.failOnFailure === true || action.params.failOnFailure === "true")){
+		throw build;
+	}
+
+	return build;
+}
 
 module.exports = {
 	buildJob : buildJob
